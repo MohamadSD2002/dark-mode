639a5a995b32efdc356b1d79b535b368601ad83f
Mohamad Bakhshaish <118728086+MohamadSD2002@users.noreply.github.com>
2024-11-12 17:53:56 +0330

    add styles in index.js component

--- src/index.js
+++ src/index.js
@@ -2,6 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import { BrowserRouter as Router } from 'react-router-dom';
+import './styles/index.less';
+import './styles/responsive.less';
+import './styles/themeVariables.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
