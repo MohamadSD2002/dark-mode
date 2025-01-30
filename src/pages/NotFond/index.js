61bd2b4fe646d3bff43711c93330b33e1b997318
Mohamad Bakhshaish <118728086+MohamadSD2002@users.noreply.github.com>
2025-01-31 02:51:19 +0330

    Delete src directory

--- src/pages/NotFond/index.js
+++
@@ -1,9 +0,0 @@
-const NotFond = () => {
-  return (
-    <div className="not-fond">
-      <h1>404 NotFound page!...</h1>
-    </div>
-  )
-}
-
-export default NotFond;
--- src/tools/theme.js
+++
@@ -1,4 +0,0 @@
-export const setTheme = (theme) => {
-    const htmlTag = document.documentElement;
-    htmlTag.setAttribute("theme", theme);
-};
